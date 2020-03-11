0f695536dda03753c7db02959d5a1f47c0f770f4
chanceclay61 <61808813+chanceclay61@users.noreply.github.com>
2020-03-11 16:39:48 -0500

    Add files via upload

---
+++ postalSuite.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+driver = webdriver.Chrome(r"C:\Users\chanc\PycharmProjects\Automation\drivers\chromedriver.exe")
+driver.get("https://store.usps.com/store/home")
+
+driver.find_element_by_id("login-register-header").click()
+driver.implicitly_wait(10)
+driver.find_element_by_id("username").send_keys(("chanceclay61"))
+driver.find_element_by_id("password").send_keys("Chance0176!")
+driver.find_element_by_id("btn-submit").click()
